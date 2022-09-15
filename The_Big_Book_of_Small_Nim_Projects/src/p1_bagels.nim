3c243a0a10fdc5c4102dfb3063aa0b1e20a64068
Nicola Muneratto <nicola.muneratto@statnett.no>
2022-09-15 14:50:35 +0200

    birthday paradox program

--- The_Big_Book_of_Small_Nim_Projects/src/p1_bagels.nim
+++ The_Big_Book_of_Small_Nim_Projects/src/p1_bagels.nim
@@ -1,9 +1,6 @@
 #[Bagels, by Al Sweigart al@inventwithpython.com
-  2. A deductive logic game where you must guess a number based on clues.
-  3. This code is available at https://nostarch.com/big-book-small-python-programming
-  4. A version of this game is featured in the book, "Invent Your Own
-  5. Computer Games with Python" https://nostarch.com/inventwithpython
-  6. Tags: short, game, puzzle]#
+  A deductive logic game where you must guess a number based on clues.
+  A version of this game is featured in the book, "Invent Your Own Computer Games with Python" https://nostarch.com/inventwithpython]#
 
 import algorithm
 import random
